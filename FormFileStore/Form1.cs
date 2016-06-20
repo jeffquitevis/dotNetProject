4810820da3fbcbd0083632668705c83088abe9bc
jeffquitevis <jeffquitevis@yahoo.ca>
2016-06-19 23:54:32 -0700

    Add project files.

---
+++ FormFileStore/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Model;
+
+namespace FormFileStore
+{
+    public partial class Form1 : Form
+    {
+        private PersonStoreController psc = new PersonStoreController(new DataStoreFile());
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void bAdd_Click(object sender, EventArgs e)
+        {
+            
+            psc.Add(new Person(Convert.ToInt64(tbId.Text),tbFirstName.Text, tbLastName.Text ));
+
+        }
+
+        private void bSearch_Click(object sender, EventArgs e)
+        {
+            
+            Person tempPerson = psc.Search(Convert.ToInt64(tbSearchId.Text));
+
+            rtbSearchResult.AppendText(tempPerson.FirstName);
+         
+        }
+
+        private void bDelete_Click(object sender, EventArgs e)
+        {
+            psc.Delete(Convert.ToInt64(tbDelete.Text));
+        }
+    }
+}
---
+++ FormFileStore/IDataStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace FormFileStore
+{
+    public interface IDataStore
+    {
+
+        void AddPerson(Person person);
+        Person SearchPerson(long id);
+        void DeletePerson(long id);
+    }
+}
