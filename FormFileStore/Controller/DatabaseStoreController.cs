88eaab1e9cb0abf5f5082e085311b782ac598c69
jeffquitevis <jeffquitevis@yahoo.ca>
2016-06-25 01:43:13 -0700

    Added DataBase

---
+++ FormFileStore/Controller/DatabaseStoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FormFileStore.Model;
+
+namespace FormFileStore.Controller
+{
+    class DatabaseStoreController
+    {
+
+        private IDataStore dataStore;
+
+        public DatabaseStoreController(IDataStore dataStore)
+        {
+            this.dataStore = dataStore;
+        }
+
+        public void Insert(Person person)
+        {
+            dataStore.AddPerson(person);
+        }
+
+        public Person Select(long id)
+        {
+            return dataStore.SearchPerson(id);
+        }
+    }
+}
