19e40cc469e96dfa7fcf7d7a3e55fb23761bfe77
jeffquitevis <jeffquitevis@yahoo.ca>
2016-06-25 01:43:13 -0700

    Added DataBase

---
+++ FormFileStore/DataStoreDatabase.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FormFileStore.Model;
+using System.Data;
+
+namespace FormFileStore
+{
+    class DataStoreDatabase : IDataStore
+    {
+
+        private string connectionString = @"Data Source=JEFFSTEPHEN\SQLEXPRESS;Initial Catalog=Person;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+        public DataStoreDatabase()
+        {
+            
+        }
+
+        public void AddPerson(Person person)
+        {
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                ;
+                SqlDataAdapter insert = new SqlDataAdapter();
+                insert.InsertCommand = new SqlCommand("Insert into Person (PersonId,FirstName,LastName) Values (@Id,@Fname,@Lname)",connection);
+                insert.InsertCommand.Parameters.AddWithValue("@Id",person.Id);
+                insert.InsertCommand.Parameters.AddWithValue("@Fname", person.FirstName);
+                insert.InsertCommand.Parameters.AddWithValue("@Lname", person.LastName);
+                insert.InsertCommand.ExecuteNonQuery();
+
+            }
+       
+        }
+
+        public Person SearchPerson(long id)
+        {
+            Person tempPerson = null;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+              
+               
+                connection.Open();               
+                SqlDataAdapter select = new SqlDataAdapter();
+                select.SelectCommand = new SqlCommand("Select * from Person WHERE PersonID = @Id",connection);
+                select.SelectCommand.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = select.SelectCommand.ExecuteReader();
+                
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                         tempPerson = new Person((long) Convert.ToUInt64(reader["PersonID"].ToString()),reader["FirstName"].ToString(),reader["LastName"].ToString());
+                    }
+                   
+                }
+                
+                reader.Close();
+
+            }
+
+            return tempPerson;
+        }
+
+        public void DeletePerson(long id)
+        {
+          
+        }
+    }
+}
--- FormFileStore/Form/Form1.cs
+++ FormFileStore/Form/Form1.cs
@@ -6,7 +6,8 @@ namespace FormFileStore.Form
 {
     public partial class Form1 : System.Windows.Forms.Form
     {
-        private PersonStoreController psc = new PersonStoreController(new DataStoreFile());
+        //private PersonStoreController psc = new PersonStoreController(new DataStoreFile());
+        private DatabaseStoreController dsc = new DatabaseStoreController(new DataStoreDatabase());
 
         public Form1()
         {
@@ -16,30 +17,32 @@ namespace FormFileStore.Form
 
         private void bAdd_Click(object sender, EventArgs e)
         {
-            
-            psc.Add(new Person(Convert.ToInt64(tbId.Text),tbFirstName.Text, tbLastName.Text ));
-            tbId.Clear();
-            tbFirstName.Clear();
-            tbLastName.Clear();
+
+            dsc.Insert(new Person(Convert.ToInt64(tbId.Text), tbFirstName.Text, tbFirstName.Text));
+
+            //psc.Add(new Person(Convert.ToInt64(tbId.Text),tbFirstName.Text, tbLastName.Text ));
+            //tbId.Clear();
+            //tbFirstName.Clear();
+            //tbLastName.Clear();
 
         }
 
         private void bSearch_Click(object sender, EventArgs e)
         {
             
-            Person tempPerson = psc.Search(Convert.ToInt64(tbSearchId.Text));
+            Person tempPerson = dsc.Select(Convert.ToInt64(tbSearchId.Text));
 
             rtbSearchResult.Clear();
             rtbSearchResult.AppendText("ID: " + tempPerson.Id);
             rtbSearchResult.AppendText("\n");
             rtbSearchResult.AppendText("FIRSTNAME: " + tempPerson.FirstName);
             rtbSearchResult.AppendText("\n");
-            rtbSearchResult.AppendText("LASTNAME: "+ tempPerson.LastName);     
+            rtbSearchResult.AppendText("LASTNAME: " + tempPerson.LastName);
         }
 
         private void bDelete_Click(object sender, EventArgs e)
         {
-            psc.Delete(Convert.ToInt64(tbDelete.Text));
+            //psc.Delete(Convert.ToInt64(tbDelete.Text));
         }
     }
 }
